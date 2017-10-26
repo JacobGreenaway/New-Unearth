5f018eed127f57bd35c170f124b9818a18bfb1d8
DESKTOP-VKGC944\En Bo <enbo@stpaulskogarah.com.au>
2017-10-27 09:43:47 +1100

    Created simple timer class Use simple timer to cause animals to change directions from time to time

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -8,33 +8,49 @@ public class Animal : MonoBehaviour {
     public bool waterAble;
 
     //The current direction the animal is engaged in (1, 2, 3, 4, 5, 6, 7, 8 With 5 been standing still)
-    private byte _direction;
+    private int _direction;
 
     //The amount of time that lapses before it changes direction
     public int restless;
 
+    private SimpleTimer timer;
+
 	// Use this for initialization
 	void Start () {
+        timer = GetComponent<SimpleTimer>();
+        //timer.targetTime = 
         _direction = 1;
     }
+
+
 	
 	// Update is called once per frame
 	void Update () {
+        if (timer.Count() == 0f)
+        {
+            ChangeDirection();
+        }
         Move();
-	}
+    }
+
+    private void ChangeDirection()
+    {
+        _direction = Random.Range(0, 10);
+
+    }
 
     private void Move ()
     {
         switch (_direction)
         {
             case 1:
-                transform.Translate(-1, 0, -1);
+                transform.Translate(-1, -1, 0);
                 break;
             case 2:
-                transform.Translate(1, 0, -1);
+                transform.Translate(1, -1, 0);
                 break;
             case 3:
-                transform.Translate(1, 0, -1);
+                transform.Translate(1, -1, 0);
                 break;
             case 4:
                 transform.Translate(-1, 0, 0);
@@ -46,13 +62,13 @@ public class Animal : MonoBehaviour {
                 transform.Translate(1, 0, 0);
                 break;
             case 7:
-                transform.Translate(-1, 0, 1);
+                transform.Translate(-1, 1, 0);
                 break;
             case 8:
-                transform.Translate(0, 0, 1);
+                transform.Translate(0, 1, 0);
                 break;
             case 9:
-                transform.Translate(1, 0, 1);
+                transform.Translate(1, 1, 0);
                 break;
         }
         
---
+++ Assets/Scripts/SimpleTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTimer : MonoBehaviour
+{
+
+    public float targetTime = 60.0f;
+    public float currentTime;
+
+    void Start()
+    {
+        currentTime = targetTime;
+    }
+    //Returns time left or zero
+    //Upon returning zero, will reset
+    public float Count()
+    {
+
+        currentTime -= Time.deltaTime;
+
+        if (currentTime <= 0.0f)
+        {
+            currentTime = targetTime;
+            return 0f;
+        }
+        return currentTime;
+    }
+
+    void timerEnded()
+    {
+        //do your stuff here.
+    }
+
+}
