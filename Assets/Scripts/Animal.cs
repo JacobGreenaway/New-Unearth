e132e661e3a971738a90c88fcd3cd006b1dcab8b
James Murchison <jamesmurchison83@gmail.com>
2017-12-04 14:19:20 +1100

    Changed animal movement code to be frame rate independant.

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -7,7 +7,8 @@ public class Animal : MonoBehaviour {
     public bool landAble;
     public bool waterAble;
 
-    
+    [SerializeField]
+    private float speed = 0.5f;
 
     //The current direction the animal is engaged in (1, 2, 3, 4, 5, 6, 7, 8 With 5 been standing still)
     private int _direction;
@@ -59,44 +60,54 @@ public class Animal : MonoBehaviour {
 
     private void Move ()
     {
+        Vector3 dir = new Vector3(0f, 0f, 1f);
         switch (_direction)
         {
             case 1:
-                transform.Translate(-1, 0, -1, Space.World);
+                //transform.Translate(-1, 0, -1, Space.World);
+                dir = Quaternion.AngleAxis(225f, Vector3.up) * dir;
                 transform.eulerAngles = new Vector3(90, -135, 0);
                 break;
             case 2:
-                transform.Translate(0, 0, -1, Space.World);
+                //transform.Translate(0, 0, -1, Space.World);
+                dir = Quaternion.AngleAxis(270f, Vector3.up) * dir;
                 transform.eulerAngles = new Vector3(90, 180, 0);
                 break;
             case 3:
-                transform.Translate(1, 0, -1, Space.World);
+                //transform.Translate(1, 0, -1, Space.World);
+                dir = Quaternion.AngleAxis(135f, Vector3.up) * dir;
                 transform.eulerAngles = new Vector3(90, -225, 0);
                 break;
             case 4:
-                transform.Translate(-1, 0, 0, Space.World);
+                //transform.Translate(-1, 0, 0, Space.World);
+                dir = Quaternion.AngleAxis(270f, Vector3.up) * dir;
                 transform.eulerAngles = new Vector3(90, -90, 0);
                 break;
             case 5:
                 //Stand still
+                dir = Vector3.zero;
                 break;
             case 6:
-                transform.Translate(1, 0, 0, Space.World);
+                //transform.Translate(1, 0, 0, Space.World);
+                dir = Quaternion.AngleAxis(90f, Vector3.up) * dir;
                 transform.eulerAngles = new Vector3(90, 90, 0);
                 break;
             case 7:
-                transform.Translate(-1, 0, 1, Space.World);
+                //transform.Translate(-1, 0, 1, Space.World);
+                dir = Quaternion.AngleAxis(315f, Vector3.up) * dir;
                 transform.eulerAngles = new Vector3(90, -45, 0);
                 break;
             case 8:
-                transform.Translate(0, 0, 1, Space.World);
+                //transform.Translate(0, 0, 1, Space.World);
+                // This is our default dir vector
                 transform.eulerAngles = new Vector3(90, 0, 0);
                 break;
             case 9:
-                transform.Translate(1, 0, 1, Space.World);
+                //transform.Translate(1, 0, 1, Space.World);
+                dir = Quaternion.AngleAxis(45f, Vector3.up) * dir;
                 transform.eulerAngles = new Vector3(90, 45, 0);
                 break;
         }
-        
+        transform.position += dir * Time.deltaTime;
     }
 }
